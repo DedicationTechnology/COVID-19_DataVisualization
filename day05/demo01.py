a26d3254e60eac89539813d7c8849bef719f1855
splitSecond <2358241108@qq.com>
2020-07-08 12:14:24 +0800

    学校python培训第五天学习

---
+++ day05/demo01.py
@@ -0,0 +1,14 @@
+"""当前路径的获取"""
+# import os
+#
+# path = os.getcwd()  # getcwd()命令可以获得当前所在的路径
+# print(path)
+
+"""从包中导入函数"""
+# from math import sin
+#
+# # print(math.sin(3))  # 导入具体的函数后，不能再用包名.函数名的形式调用函数
+# print(sin(3))
+
+import zhidingyi
+zhidingyi.zhu()
